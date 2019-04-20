574377ab978e48581000bc565636a20f2f0f907a
Mikhail Mahutsiy <magut@ukr.net>
2019-04-21 00:55:44 +0200

    new proj add web, Thymeleaf

---
+++ src/main/java/udm/spg/mvc/SpgmvcApplication.java
@@ -0,0 +1,13 @@
+package udm.spg.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udm/spg/mvc/SpgmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package udm.spg.mvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpgmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
